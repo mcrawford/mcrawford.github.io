4680ac75e7f8b8577130bee030b746652dab74bb
Matt Crawford <mcrawford@dayspring-tech.com>
2020-06-16 08:14:40 -0700

    add

--- album-picker-playlist/app/data/current-album-ids.ts
+++ album-picker-playlist/app/data/current-album-ids.ts
@@ -65,5 +65,6 @@ export const currentAlbumIds = [
     'spotify:album:3QSRtkfas8Gr0vDByhFvkE',
     'spotify:album:3sF4iIrRTsRNKC6AMR6D2d',
     'spotify:album:2TuXIRGEbUFfakkBDXMLN1',
-    'spotify:album:082Lg7YaKtzsScelrtfwdx'
+    'spotify:album:082Lg7YaKtzsScelrtfwdx',
+    'spotify:album:0fO1KemWL2uCCQmM22iKlj'
 ];
